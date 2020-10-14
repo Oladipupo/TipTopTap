992efe7a81e803a5e1a8dc9f1403c42276c0313d
Mid2oceans <46832094+Mid2oceans@users.noreply.github.com>
2020-10-13 22:33:02 -0400

    Made the cutscenes

---
+++ TipTapTopFinal/Assets/Scripts/dialog.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class dialog : MonoBehaviour
+{
+
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    public AudioClip[] sentencesAudio;
+    public AudioSource audioSource;
+    private int index;
+    public float typingSpeed;
+    private bool allowClick = false;
+    public string nextLevelName;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(textDisplay.text == sentences[index])
+        {
+            allowClick = true;
+        }
+        if (allowClick ==true && Input.anyKeyDown)
+        {
+            nextSentence();
+        }
+    }
+
+
+    IEnumerator Type()
+    {
+        audioSource.clip = sentencesAudio[index];
+        audioSource.Play();
+
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void nextSentence()
+    {
+        allowClick = false;
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            Application.LoadLevel(nextLevelName);
+        }
+    }
+
+    public void skipIt()
+    {
+        Application.LoadLevel(nextLevelName);
+    }
+}
