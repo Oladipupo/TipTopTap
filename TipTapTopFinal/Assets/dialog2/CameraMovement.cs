b91c46d477dc0e2a607f34285f11a93d2f469993
Mid2oceans <46832094+Mid2oceans@users.noreply.github.com>
2020-10-15 21:16:21 -0400

    cutscene2 done just needs Ladis voice

---
+++ TipTapTopFinal/Assets/dialog2/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Camera mainCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnimateDown();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AnimateDown()
+    {
+        
+        while (mainCam.transform.rotation.x < 33)
+        {
+            mainCam.transform.Rotate(1.0f, 0.0f, 0.0f);
+        }
+    }
+}
