5cc93e4336b6f75f5aff2873a8b6ad61b756b422
Oladipupo Ogundipe <Oladipupo.Ogundipe@gmail.com>
2020-11-06 02:49:36 -0500

    Demo 11/06

--- TipTapTopFinal/Assets/Scripts/noteCatcher.cs
+++ TipTapTopFinal/Assets/Scripts/noteCatcher.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class noteCatcher : MonoBehaviour
 
@@ -9,16 +11,27 @@ public class noteCatcher : MonoBehaviour
 
     public AudioSource audio;
     public AudioClip miss;
+    public string currentLevel;
+    public int count = 0;
+    public float waitTime;
+    public Text text;
     // Start is called before the first frame update
     void Start()
     {
+        waitTime = 4f;
         audio = GetComponent<AudioSource>();
+        text.text = " ";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+        if(count == 35){  
+             StartCoroutine(waiter());
+            }
+            
+            
     }
 
     private void OnTriggerEnter(Collider other)
@@ -26,9 +39,17 @@ public class noteCatcher : MonoBehaviour
         audio.clip = miss;
         audio.Play();
 
+        count++;
+
         Destroy(other.gameObject);
         sceneController.GetComponent<SceneController>().updateScore(-10);
         sceneController.GetComponent<SceneController>().inARow = 0;
 
+    }
+
+    IEnumerator waiter(){
+        text.text = "MISSED TOO MANY NOTES. RESTARTING";
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(currentLevel);
     }
 }
